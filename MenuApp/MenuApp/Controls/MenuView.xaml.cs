0f7beb50ee2902dfa7a8a1a29a39a9415cc5ccef
FRANK ROBERTO CHAVEZ SOSA <1085268@est.intec.edu.do>
2021-09-08 13:37:32 -0400

    First commit

---
+++ MenuApp/MenuApp/Controls/MenuView.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MenuApp.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuView : ContentView
+    {
+        public MenuView()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty MenuTitleProperty = BindableProperty.Create(
+            "MenuTitle",        // the name of the bindable property
+            typeof(string),     // the bindable property type
+            typeof(MenuView),   // the parent object type
+            string.Empty);      // the default value for the property
+
+        public static readonly BindableProperty MenuIconSourceProperty = BindableProperty.Create(
+            "MenuIconSource",        // the name of the bindable property
+            typeof(string),     // the bindable property type
+            typeof(MenuView),   // the parent object type
+            string.Empty);      // the default value for the property
+
+        public static readonly BindableProperty MenuDescriptionProperty = BindableProperty.Create(
+            "MenuDescription",        // the name of the bindable property
+            typeof(string),     // the bindable property type
+            typeof(MenuView),   // the parent object type
+            string.Empty);      // the default value for the property
+
+        public string MenuTitle
+        {
+            get => (string)GetValue(MenuTitleProperty);
+            set => SetValue(MenuTitleProperty, value);
+        }
+
+        public string MenuIconSource
+        {
+            get => (string)GetValue(MenuIconSourceProperty);
+            set => SetValue(MenuIconSourceProperty, value);
+        }
+
+        public string MenuDescription
+        {
+            get => (string)GetValue(MenuDescriptionProperty);
+            set => SetValue(MenuDescriptionProperty, value);
+        }
+    }
+}
